1687edf4b1aef241714a8eaa097cd2c4e61cc045
Makers Academy <onboarding@makersacademy.com>
2018-10-09 10:09:01 +0000

    Add test for add_note method

---
+++ lib/notes.rb
@@ -0,0 +1,4 @@
+class Note
+    def add_note
+    end
+end
--- spec/notes_spec.rb
+++ spec/notes_spec.rb
@@ -1,3 +1,7 @@
-describe notes do
-    
+require 'notes'
+
+describe Note do 
+  it 'responds to add_note' do
+    expect(subject).to respond_to :add_note
+  end
 end
